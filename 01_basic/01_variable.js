82a62b4bdd28c7b73c94bed8d2ff5ab24bdf022e
neha0977 <123052902+neha0977@users.noreply.github.com>
2024-10-21 08:47:11 +0000

    datatypes

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,20 @@
+const id = 5656 //value does not change, fix teh value
+let email = 'example.com'
+var password = "12345"
+city = "Noida"
+
+// id = 789 // not allowed 
+
+email = "neha@gmail.com"
+password ="200000"
+city='Delhi'
+
+console.log(id);
+
+console.table([id,email,password,city])
+
+// {} scope
+/*
+prefer not to use var cause of the issue in block scope and functional scope
+
+*/
---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all js code as newer version
+
+// alert("Helo") //we are uisng node js not browser
+
+let  name = "neha"
+let age = 22
+let isStatus = false
+
+// / number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value representation of the empty value
+// undefined => variable initilised bt value not defined , it is in type itself spetial type 
+// symbol => unique find the uniquness 
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
