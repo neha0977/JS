a5ded402ad280fc371125424b220193cfba65a56
neha0977 <123052902+neha0977@users.noreply.github.com>
2024-11-05 09:28:43 +0000

    string

---
+++ 01_basic/datatypes-summary.js
@@ -0,0 +1,59 @@
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+
+// ++++++++++++++++++++++++++++++++
+// Stack (Primitive)
+// Heap (Non-Primitive
+
+let name = "Neha"
+let anothename ="Garima"
+console.log(name);
+console.log(anothename);
+
+let userOne = {
+    id:1,
+    email:'ss@wxample.com'
+}
+
+let userTwo = userOne
+userTwo.email = "Neha@example.com"
+console.log(userOne);
+console.log(userTwo);
